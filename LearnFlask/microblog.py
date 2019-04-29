f0e5f80b74cedc252a1f3ce45d6b6ba49764e674
vsanjeevi <sanjeevi.official@gmail.com>
2019-04-29 11:52:17 +0530

    Delete microblog.py

--- LearnFlask/microblog.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-# programmatic does not seem to work
-# have to  $ . venv/bin/activate  | $ sudo bash to get access to port 80.
-# better way is to use CLI :
-#       $ flask run --host=0.0.0.0 --port=80
-#if __name__ == '__main__':
-#      app.run(host='0.0.0.0', port=80)
