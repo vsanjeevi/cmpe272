54d5bb550ffc347f029cbf8f79461e92bdb7ce9b
vsanjeevi <sanjeevi.official@gmail.com>
2019-04-29 11:05:51 +0530

    Add files via upload

---
+++ pop-sample-unittest.py
@@ -0,0 +1,26 @@
+import unittest
+import mongomock
+import json
+import popsample 
+
+class DBTests(unittest.TestCase):
+	def setUp(self):
+		self.db = mongomock.MongoClient()['testdb']
+		self.cust_rec1=[{"_id" : 5, "FirstName" : "Melon", "LastName" : "Seed"},
+				{"_id" : 6, "FirstName" : "Apple", "LastName" : "Seed"},
+				{"_id" : 7, "FirstName" : "Orange", "LastName" : "Seed"},
+				{"_id" : 8, "FirstName" : "Grape", "LastName" : "Seed"},
+				{"_id" : 9, "FirstName" : "Berry", "LastName" : "Seed"}]
+ 
+	def tearDown(self):
+        	pass
+
+	def test_cust_add(self):
+		ret=popsample.add_cust(self.db,self.cust_rec1)
+		print("Added Customer with ID --",ret.inserted_ids)
+		self.assertEqual(len(ret.inserted_ids), 5)
+		
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ popsample.py
@@ -0,0 +1,20 @@
+import sys
+import pymongo
+
+def add_cust(db,cust_rec1):
+	collection=db["customer"]
+	insert_cust=collection.insert_many(cust_rec1)
+	return(insert_cust)
+
+if __name__ == "__main__":
+	argv = sys.argv
+	if len(argv) < 2:
+		print("Usage: python add_customer.py mongodb_uri")
+		exit(-1)
+
+	mongodb_uri = argv[1]
+
+	db = pymongo.MongoClient(mongodb_uri).get_database()
+
+	for customer in add_cust(db):
+	  print("Customer Added --",customer['FirstName'])
